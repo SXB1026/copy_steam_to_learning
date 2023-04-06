2290bff961ecb821e1a5d7b17d680737f73ca510
sun <2716371768@qq.com>
2023-04-06 17:11:15 +0800

    updata something to level2

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const port = 3000;
 //在写图片路径时可以直接把public视为根目录
 app.use(express.static('public'));
 
+// 设置 views 文件夹作为模板文件的存放位置
+// app.set('views', path.join(__dirname, 'views'));
 
 // 设置 EJS 作为模板引擎
 app.set('view engine', 'ejs');
@@ -57,6 +59,20 @@ app.get('/data/:index/:limit', (req, res) => {
       res.json(results);//将查询到的结果 results 转换为 JSON 格式，并将其设置为响应体，同时设置响应的 Content-Type 为 "application/json"。
     }
   });
+});
+
+
+app.get('/storeroom', (req, res) => {
+  res.render('storeroom');
+});
+
+app.get('/community', (req, res) => {
+  res.render('community');
+});
+app.get('/details/:id', (req, res) => {
+  const itemId = req.params.id;
+  // 可以根据 itemId 从数据库中检索项目详细信息，然后将其传递给模板。
+  res.render('details', { id: itemId });
 });
 
 
