75dfd36493385a3546efa791b3a5cd40c0bc91dd
sun <2716371768@qq.com>
2023-04-11 09:03:43 +0800

    改进

--- routes/gameCategoryRoutes.js
+++ routes/gameCategoryRoutes.js
@@ -3,16 +3,7 @@ const router = express.Router();
 const itemsModel = require("../models/itemsModel");
 const { getAllGamesByCategory} = require("../models/itemsModel");
 
-router.get("/:category", async (req, res) => {
-  try {
-    const category = req.params.category;
-    const games = await itemsModel.getGamesByCategory(category);
-    res.render("game-category", { games });
-  } catch (err) {
-    console.error("Error fetching games by category:", err);
-    res.status(500).send("Internal Server Error");
-  }
-});
+
 
 
 
